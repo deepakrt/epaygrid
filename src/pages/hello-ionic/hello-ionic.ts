2c2324301f0f168dc886a160e31b7769686890c3
deepakrt <deepak.pfcl@gmail.com>
2017-08-23 00:46:44 +0530

    adding some files

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+/**
+ * The Welcome Page is a splash page that quickly describes the app,
+ * and then directs the user to create an account or log in.
+ * If you'd like to immediately put the user onto a login/signup page,
+ * we recommend not using the Welcome page.
+*/
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+
+export class HelloIonicPage {
+  constructor() {
+
+  }
+}
